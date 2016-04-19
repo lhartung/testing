d17aa8b0274e2361acb15fa4ea447c2085978647
Lance Hartung <lance@exis.io>
2016-04-19 15:39:23 -0500

    testing

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var riffle = require('jsriffle');
 
-riffle.setFabric(process.env.WS_URL);
-riffle.setLogLevelInfo();
+var soap = require('soap');
 
 var domain = riffle.Domain(process.env.DOMAIN);
+
+riffle.setFabric(process.env.WS_URL);
+
+riffle.setLogLevelInfo();
+
+
+
+
 
 domain.onJoin = function() {
     this.register("echo", riffle.want(function(msg) {
